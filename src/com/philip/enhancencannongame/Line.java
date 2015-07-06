72118340df9135a4547b4f087b65c75b6c448345
philipogunleye <p.ogunleye@gmail.com>
2015-07-06 05:16:08 -0400

    first commit

---
+++ src/com/philip/enhancencannongame/Line.java
@@ -0,0 +1,8 @@
+package com.philip.enhancencannongame;
+
+import android.graphics.Point;
+
+public class Line {
+	public Point start = new Point(); // start Point--(0,0) by default
+	public Point end = new Point(); // end Point--(0,0) by default
+}
